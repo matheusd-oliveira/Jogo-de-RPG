20808ebf97571b10915322bced99419643724a26
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-30 15:17:02 -0300

    separando projeto em pastas e criando classe do heroi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Heroi arus = new Heroi("Arus", 20, "Guerreiro");
+Console.Write($"{arus.nome}");
+
+
